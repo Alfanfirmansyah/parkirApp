6228d42e5f23f070b45ce666e105426aeef6dcc6
fahmyruszally <fahmyruszally@gmail.com>
2019-04-30 09:32:21 +0700

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,19 +20,13 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-xs-8 p-t-5">
-                            <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
-                            <label for="rememberme">Remember Me</label>
-                        </div>
-                        <div class="col-xs-4">
-                            <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
+                      
+                        <div class="col-md-12">
+                            <button class="btn btn-block bg-pink waves-effect" type="submit"><i class="fa fa-sign-in"></i> SIGN IN</button>
                         </div>
                     </div>
                     <div class="row m-t-15 m-b--20">
-                        <div class="col-xs-6">
-                            <a href="{{ route('register') }}">Register Now!</a>
-                        </div>
-                        <div class="col-xs-6 align-right">
+                        <div class="col-xs-12 align-right">
                             <a href="{{ route('password.request') }}">Forgot Password?</a>
                         </div>
                     </div>
